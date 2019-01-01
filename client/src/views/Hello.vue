62dc9e5d8155d67f6a714be2c9fa82a6abb9eb54
mohsin <mohsinamjad51@gmail.com>
2019-01-01 13:32:49 +0500

    1st commit

--- client/src/views/Hello.vue
+++ client/src/views/Hello.vue
@@ -12,6 +12,12 @@
 </div>
 </template>
 
+<script>
+export default {
+  name: 'Hello'
+}
+</script>
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 h1, h2 {
